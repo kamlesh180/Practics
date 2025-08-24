e45be6207adb179ad823d8a26e32fc0940dab88f
kamlesh <kamlesh81473@gmail.com>
2025-08-24 16:15:48 +0530

    add new file

---
+++ index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+ main()
+ .then(() => {
+    console.log("connection successful");
+ })
+ .catch (() => console.log(err));
+ async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+        
+ }
+ const userSchema = new mongoose.Schema({
+   name: String,
+   email:String,
+   age:Number,
+ });
+ const User = mongoose.model("User",userSchema);
+  const user1 = new User({
+   name: "Adam",
+   email: "adam@gmail.com",
+   age:48,
+  });
+  user1.save();
+  
